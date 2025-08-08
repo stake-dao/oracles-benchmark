bb70b85e36ecd8b98e42a73d5cac35f4f506600f
imqdee <qdqdqdqdqd@protonmail.com>
2025-08-08 18:51:01 +0200

    data: ETH/stETH snapshots

---
+++ test/stableswap/Snapshot_ETHstETH.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {BaseSnapshot} from "test/BaseSnapshot.sol";
+
+contract Snapshot_ETHstETH is BaseSnapshot {
+    constructor() BaseSnapshot() {
+        config.network = "mainnet";
+        config.curvePool = 0x21E27a5E5513D6e65C4f830167390997aA84843a; // ETH/stETH
+        config.directory = "stableswap/ETHstETH";
+    }
+
+    function _preDeploySetup() internal override {
+        config.sdOracleConfig.poolAssetFeeds.push(0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649); // ETH/USD
+        config.sdOracleConfig.poolAssetFeeds.push(0xCfE54B5cD566aB89272946F602D76Ea879CAb4a8); // stETH/USD
+        config.sdOracleConfig.poolAssetHeartbeats.push(1 hours);
+        config.sdOracleConfig.poolAssetHeartbeats.push(1 hours);
+
+        config.sdOracleConfig.loanAsset = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
+        config.sdOracleConfig.loanAssetFeed = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6; // USDC/USD
+        config.sdOracleConfig.loanAssetHeartbeat = 1 days;
+    }
+}

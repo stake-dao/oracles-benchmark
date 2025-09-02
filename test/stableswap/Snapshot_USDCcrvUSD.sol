d957155311b57f4676c7e81f0cc93d8e48e07622
imqdee <qdqdqdqdqd@protonmail.com>
2025-09-02 11:44:02 +0200

    data: benchmark USDC/crvUSD

---
+++ test/stableswap/Snapshot_USDCcrvUSD.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {StableSnapshot} from "test/StableSnapshot.sol";
+
+contract Snapshot_USDCcrvUSD is StableSnapshot {
+    constructor() StableSnapshot() {
+        config.network = "mainnet";
+        config.curvePool = 0x4DEcE678ceceb27446b35C672dC7d61F30bAD69E; // USDC/crvUSD
+        config.directory = "stableswap/USDCcrvUSD";
+    }
+
+    function _preDeploySetup() internal override {
+        // No need to set any feeds for this pool as coins0 is the loan asset
+        config
+            .sdOracleConfig
+            .loanAsset = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
+    }
+}

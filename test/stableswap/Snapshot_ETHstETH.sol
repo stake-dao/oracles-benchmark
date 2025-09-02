8d673bf2003c18d607dc58b3329f9a3c551f0120
imqdee <qdqdqdqdqd@protonmail.com>
2025-09-02 11:45:56 +0200

    chore: update ETH/stETH price feed

--- test/stableswap/Snapshot_ETHstETH.sol
+++ test/stableswap/Snapshot_ETHstETH.sol
@@ -11,11 +11,17 @@ contract Snapshot_ETHstETH is StableSnapshot {
     }
 
     function _preDeploySetup() internal override {
-        config.sdOracleConfig.poolAssetFeeds.push(0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649); // ETH/USD
+        config.sdOracleConfig.poolAssetFeeds.push(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        ); // ETH/USD
         config.sdOracleConfig.poolAssetHeartbeats.push(1 hours);
 
-        config.sdOracleConfig.loanAsset = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
-        config.sdOracleConfig.loanAssetFeed = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6; // USDC/USD
+        config
+            .sdOracleConfig
+            .loanAsset = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
+        config
+            .sdOracleConfig
+            .loanAssetFeed = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6; // USDC/USD
         config.sdOracleConfig.loanAssetHeartbeat = 1 days;
     }
 }

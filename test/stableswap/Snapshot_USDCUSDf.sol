7967cc0aaebb54ecb80ee0e76f3d5ee0790e5653
imqdee <qdqdqdqdqd@protonmail.com>
2025-09-02 11:44:52 +0200

    data: benchmark USDC/USDf

---
+++ test/stableswap/Snapshot_USDCUSDf.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {StableSnapshot} from "test/StableSnapshot.sol";
+
+contract Snapshot_USDCUSDf is StableSnapshot {
+    constructor() StableSnapshot() {
+        config.network = "mainnet";
+        config.curvePool = 0x72310DAAed61321b02B08A547150c07522c6a976; // USDC/USDf
+        config.directory = "stableswap/USDCUSDf";
+    }
+
+    function _preDeploySetup() internal override {
+        // No need to set any feeds for this pool as coins0 is the loan asset
+        config
+            .sdOracleConfig
+            .loanAsset = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
+    }
+}
